e3e62d10b08a95ecf261e67f7219e8cd4046f73d
judaondo <jonuda77@gmail.com>
2021-05-29 00:55:12 +0200

    [vagrant] k8s-tools provisioner fixed

--- provision/k8s_tools.sh
+++ provision/k8s_tools.sh
@@ -35,11 +35,11 @@ fi
 if [ ! -f /usr/local/bin/k9s ]; then
 
   cd /tmp
-  wget https://github.com/derailed/k9s/releases/download/$K9S_VERSION/k9s_Linux_x86_64.tar.gz
-  tar -zxvf k9s_Linux_x86_64.tar.gz
+  wget https://github.com/derailed/k9s/releases/download/${K9S_VERSION}/k9s_${K9S_VERSION}_Linux_x86_64.tar.gz
+  tar -zxvf k9s_${K9S_VERSION}_Linux_x86_64.tar.gz
   cp k9s /usr/local/bin/k9s
 
-  rm -rf k9s_Linux_x86_64.tar.gz k9s LICENCE README.md
+  rm -rf k9s_${K9S_VERSION}_Linux_x86_64.tar.gz k9s LICENCE README.md
   echo "k9s Version --> $(k9s version)"
 
   echo "--> k9s successfully installed."
