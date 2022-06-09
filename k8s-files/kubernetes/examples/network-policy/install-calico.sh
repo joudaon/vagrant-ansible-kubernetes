f24baa9ee7060932554c78846ce240ce19a1a7ef
joudaon <jonuda77@gmail.com>
2022-06-09 17:25:50 +0200

    [network-policies] apps isolated in namespaces

--- k8s-files/kubernetes/examples/network-policy/install-calico.sh
+++ k8s-files/kubernetes/examples/network-policy/install-calico.sh
@@ -6,3 +6,7 @@ sleep 30s
 
 # Verify Calico installation in your cluster by issuing the following command.
 # watch kubectl get pods -l k8s-app=calico-node -A
+
+# Install calico - https://projectcalico.docs.tigera.io/maintenance/clis/calicoctl/install
+curl -L https://github.com/projectcalico/calico/releases/download/v3.23.1/calicoctl-linux-amd64 -o calicoctl
+chmod +x ./calicoctl
