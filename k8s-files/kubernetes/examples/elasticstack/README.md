75d0e692193ed64957eeb90390474f540fbdf36a
joudaon <jonuda77@gmail.com>
2022-12-22 18:21:37 +0100

    [elasticstack] elasticstack created

---
+++ k8s-files/kubernetes/examples/elasticstack/README.md
@@ -0,0 +1,16 @@
+## Requirements
+
+- minikube
+- helm
+
+## Setup
+
+1. Run `install.sh` script.
+
+## Useful information:
+
+- [Github - Elastic Helm charts](https://github.com/elastic/helm-charts)
+- [Helm - Kibana](https://artifacthub.io/packages/helm/elastic/kibana#installing)
+- [Kubernetes ElasticSearch with Helm minikube](https://www.bogotobogo.com/DevOps/Docker/Docker_Kubernetes_ElasticSearch_with_Helm_minikube.php)
+- [Use Helm 3 to Install ELK Stack on Minikube in 10 Mins](https://www.youtube.com/watch?v=ObLXSMfDX1o)
+- [Filebeat + Elk Stack Tutorial With Kubernetes](https://www.youtube.com/watch?v=SU--XMhbWoY)
---
+++ k8s-files/kubernetes/examples/elasticstack/install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+IMAGE_VERSION=8.5.1
+
+## Set docker as default driver
+minikube config set driver docker
+
+## Start Minikube with a built-in Calico implementation, this is a quick way to checkout Calico features.
+minikube start --memory 8192 --cpus 4 # --nodes 2
+
+## Install kube-state-metrics
+echo "--> Installing kube-state-metrics"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/kube-state-metrics/master/examples/standard/cluster-role-binding.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/kube-state-metrics/master/examples/standard/cluster-role.yaml 
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/kube-state-metrics/master/examples/standard/deployment.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/kube-state-metrics/master/examples/standard/service-account.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/kube-state-metrics/master/examples/standard/service.yaml
+
+## Install Elasticsearch
+echo "--> Installing Elasticsearch with Helm"
+helm repo add elastic https://helm.elastic.co
+helm repo list
+helm install elasticsearch elastic/elasticsearch --values elastic-values.yaml --namespace=elk --create-namespace --wait --set imageTag=$IMAGE_VERSION
+
+##  Install Kibana
+echo "--> Installing Kibana with Helm"
+helm install kibana elastic/kibana --namespace=elk --wait --set imageTag=$IMAGE_VERSION
+kubectl expose deployment kibana-kibana --type=NodePort --name=kibana-service --namespace=elk
+
+##  Install Filebeat
+echo "--> Installing Filebeat with Helm"
+helm install filebeat elastic/filebeat --namespace=elk --wait --set imageTag=$IMAGE_VERSION
+
+##  Install Metricbeat
+echo "--> Installing Metricbeat"
+kubectl apply -f metricbeat.yaml --wait=true
+sleep 20s
+# echo "--> Installing Metricbeat with Helm"
+# helm install metricbeat elastic/metricbeat --namespace=elk --wait
+
+## Load Metricbeat dashboards
+echo "--> Loading Kibana Dashboards"
+METRICBEAT_POD=$(kubectl get pod -n elk | grep metricbeat | awk '{ print $1 }')
+kubectl exec -it $METRICBEAT_POD -n elk -- /bin/bash -c "metricbeat setup --dashboards -E output.elasticsearch.hosts=['elasticsearch-master:9200'] -E setup.kibana.host=kibana-kibana:5601"
+sleep 10s
+
+# Get credentials
+echo "--> Installation finished!"
+NODEPORT=$(kubectl get service kibana-service --namespace=elk -ojsonpath='{.spec.ports[0].nodePort}')
+ELASTIC_PASSWORD="$(kubectl get secrets --namespace=elk elasticsearch-master-credentials -ojsonpath='{.data.password}' | base64 -d)"
+echo "Kibana URL --> $(minikube ip):$NODEPORT"
+echo "Kibana username --> elastic"
+echo "Kibana password --> $ELASTIC_PASSWORD"
+
+## Export port
+# kubectl port-forward deployment/kibana-kibana 5601 -n elk
+
